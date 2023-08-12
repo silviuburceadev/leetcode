ca32bfbdfd4aa120dde0cbbf30339949f1d6d30b
Silviu BURCEA <silviuburceadev@gmail.com>
2023-08-12 13:50:55 +0300

    TDD/GREEN - Change implementation to fix the test

--- main/src/main/java/com/github/silviuburceadev/leetcode/main/RemoveElement.java
+++ main/src/main/java/com/github/silviuburceadev/leetcode/main/RemoveElement.java
@@ -2,6 +2,14 @@ package com.github.silviuburceadev.leetcode.main;
 
 public class RemoveElement {
     public int removeElement(int[] nums, int val) {
-        return 0;
+        int different = 0;
+        for (var i = 0; i < nums.length; i++) {
+            if (nums[i] == val) {
+                System.arraycopy(nums, i + 1, nums, i, nums.length - i);
+            } else {
+                different++;
+            }
+        }
+        return different;
     }
 }
