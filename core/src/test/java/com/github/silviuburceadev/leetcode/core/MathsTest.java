1d8c6649009d94fb2337bdb82ccb63c15a46d363
Silviu BURCEA <silviuburceadev@gmail.com>
2023-08-13 17:07:28 +0300

    Steps to zero - TDD/GREEN - Compute the total number of bits

--- core/src/test/java/com/github/silviuburceadev/leetcode/core/MathsTest.java
+++ core/src/test/java/com/github/silviuburceadev/leetcode/core/MathsTest.java
@@ -1,12 +1,15 @@
 package com.github.silviuburceadev.leetcode.core;
 
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+
+import static com.github.silviuburceadev.leetcode.core.Maths.numberOfBits;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class MathsTest {
 
     @Test
-    public void bitsOfInteger() {
-        Assertions.assertEquals(4, Maths.numberOfBits(8));
+    public void testNumberOfBits() {
+        // power of 2
+        assertEquals(4, numberOfBits(8));
     }
 }
