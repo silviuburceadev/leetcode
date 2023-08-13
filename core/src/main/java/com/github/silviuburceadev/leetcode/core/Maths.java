80a1a032bfae6d91eab8a72916eb6e56c2222a57
Silviu BURCEA <silviuburceadev@gmail.com>
2023-08-13 17:20:59 +0300

    Steps to zero - TDD/GREEN - Fixed corner case for 0

--- core/src/main/java/com/github/silviuburceadev/leetcode/core/Maths.java
+++ core/src/main/java/com/github/silviuburceadev/leetcode/core/Maths.java
@@ -3,6 +3,8 @@ package com.github.silviuburceadev.leetcode.core;
 public final class Maths {
 
     public static int numberOfBits(int n) {
+        // ln(0) is not defined
+        if (n == 0) return 1;
         return 1 + (int)(Math.log(n) / Math.log(2));
     }
 }
