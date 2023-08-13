22d10a56b31b1debfe9f3f5b8121222159e9c7ff
Silviu BURCEA <silviuburceadev@gmail.com>
2023-08-13 20:21:16 +0300

    Steps to zero - TDD/GREEN - Fixed impl. with the help of core

--- main/src/main/java/com/github/silviuburceadev/leetcode/main/StepsToZero.java
+++ main/src/main/java/com/github/silviuburceadev/leetcode/main/StepsToZero.java
@@ -1,4 +1,7 @@
 package com.github.silviuburceadev.leetcode.main;
+
+import com.github.silviuburceadev.leetcode.core.Maths;
+
 /**
  * Problem 1342
  */
@@ -6,10 +9,14 @@ public class StepsToZero {
 
     /**
      * Return the number of steps needed to reach 0 if you halve the number when even or decrement when odd.
-     * @param num   the given number
-     * @return      the number of steps
+     * <p>
+     * Counts the number of set bits, when you need to decrement, and the number of total bits, excluding
+     * the first bit, where you need to half to reach 1.
+     *
+     * @param num the given number
+     * @return the number of steps
      */
     public int numberOfSteps(int num) {
-        return Integer.bitCount(num);
+        return Maths.bitCount(num) + Integer.bitCount(num) - 1;
     }
 }
