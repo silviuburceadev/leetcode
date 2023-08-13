b82401d2d40b170579351caad07edc95b3da1fed
Silviu BURCEA <silviuburceadev@gmail.com>
2023-08-13 17:24:59 +0300

    Steps to zero - TDD/Refactor - Javadoc & naming
    - Added Javadoc to document the method and renamed it

--- core/src/main/java/com/github/silviuburceadev/leetcode/core/Maths.java
+++ core/src/main/java/com/github/silviuburceadev/leetcode/core/Maths.java
@@ -2,9 +2,17 @@ package com.github.silviuburceadev.leetcode.core;
 
 public final class Maths {
 
-    public static int numberOfBits(int n) {
+    /**
+     * Returns the minimum number of bits required to represent number <code>n</code>.
+     * <p>
+     * The implementation adds 1 to the highest power of 2 less than or equal to <code>n</code>.
+     *
+     * @param n the given number to check
+     * @return the number of bits
+     */
+    public static int bitCount(long n) {
         // ln(0) is not defined
         if (n == 0) return 1;
-        return 1 + (int)(Math.log(n) / Math.log(2));
+        return 1 + (int) (Math.log(n) / Math.log(2));
     }
 }
