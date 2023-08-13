0a01578f0332ba0ee83a023ee4909933973ed245
Silviu BURCEA <silviuburceadev@gmail.com>
2023-08-13 17:19:31 +0300

    Steps to zero - TDD/RED - Added failing test for 0

--- core/src/test/java/com/github/silviuburceadev/leetcode/core/MathsTest.java
+++ core/src/test/java/com/github/silviuburceadev/leetcode/core/MathsTest.java
@@ -18,6 +18,7 @@ public class MathsTest {
         assertEquals(7, numberOfBits(65));
         assertEquals(11, numberOfBits(1025));
         // 2^n - 1
+        assertEquals(1, numberOfBits(0));
         assertEquals(3, numberOfBits(7));
         assertEquals(6, numberOfBits(63));
         assertEquals(10, numberOfBits(1023));
