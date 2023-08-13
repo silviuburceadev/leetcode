125a4850257070d74a2d751f736850518d310bb9
Silviu BURCEA <silviuburceadev@gmail.com>
2023-08-13 17:00:17 +0300

    Steps to zero - TDD/RED - Add failing test for counting total bits

---
+++ core/src/test/java/com/github/silviuburceadev/leetcode/core/MathsTest.java
@@ -0,0 +1,12 @@
+package com.github.silviuburceadev.leetcode.core;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class MathsTest {
+
+    @Test
+    public void bitsOfInteger() {
+        Assertions.assertEquals(4, Maths.numberOfBits(8));
+    }
+}
