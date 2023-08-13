e8ab18357b7e2e02d9a4f8b1ae021e98e9874592
Silviu BURCEA <silviuburceadev@gmail.com>
2023-08-13 19:24:13 +0300

    Steps to zero - TDD/Refactor - Added docs

--- main/src/main/java/com/github/silviuburceadev/leetcode/main/StepsToZero.java
+++ main/src/main/java/com/github/silviuburceadev/leetcode/main/StepsToZero.java
@@ -1,7 +1,14 @@
 package com.github.silviuburceadev.leetcode.main;
-
+/**
+ * Problem 1342
+ */
 public class StepsToZero {
 
+    /**
+     * Return the number of steps needed to reach 0 if you halve the number when even or decrement when odd.
+     * @param num   the given number
+     * @return      the number of steps
+     */
     public int numberOfSteps(int num) {
         return Integer.bitCount(num);
     }
