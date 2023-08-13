932981f13446adda127300ad5a5ac896696148d0
Silviu BURCEA <silviuburceadev@gmail.com>
2023-08-13 17:24:59 +0300

    Steps to zero - TDD/Refactor - Javadoc & naming
    - Added Javadoc to document the method and renamed it

--- core/src/test/java/com/github/silviuburceadev/leetcode/core/MathsTest.java
+++ core/src/test/java/com/github/silviuburceadev/leetcode/core/MathsTest.java
@@ -2,25 +2,25 @@ package com.github.silviuburceadev.leetcode.core;
 
 import org.junit.jupiter.api.Test;
 
-import static com.github.silviuburceadev.leetcode.core.Maths.numberOfBits;
+import static com.github.silviuburceadev.leetcode.core.Maths.bitCount;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class MathsTest {
 
     @Test
-    public void testNumberOfBits() {
+    public void testBitCount() {
         // 2^n
-        assertEquals(4, numberOfBits(8));
-        assertEquals(7, numberOfBits(64));
-        assertEquals(11, numberOfBits(1024));
+        assertEquals(4, bitCount(8));
+        assertEquals(7, bitCount(64));
+        assertEquals(11, bitCount(1024));
         // 2^n + 1
-        assertEquals(4, numberOfBits(14));
-        assertEquals(7, numberOfBits(65));
-        assertEquals(11, numberOfBits(1025));
+        assertEquals(4, bitCount(14));
+        assertEquals(7, bitCount(65));
+        assertEquals(11, bitCount(1025));
         // 2^n - 1
-        assertEquals(1, numberOfBits(0));
-        assertEquals(3, numberOfBits(7));
-        assertEquals(6, numberOfBits(63));
-        assertEquals(10, numberOfBits(1023));
+        assertEquals(1, bitCount(0));
+        assertEquals(3, bitCount(7));
+        assertEquals(6, bitCount(63));
+        assertEquals(10, bitCount(1023));
     }
 }
