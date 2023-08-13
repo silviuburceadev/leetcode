7f7108a4ab3760e0488e6aff243c1eddf707d187
Silviu BURCEA <silviuburceadev@gmail.com>
2023-08-13 17:18:00 +0300

    Steps to zero - TDD/Refactor - Added more test cases to ensure correctness

--- core/src/test/java/com/github/silviuburceadev/leetcode/core/MathsTest.java
+++ core/src/test/java/com/github/silviuburceadev/leetcode/core/MathsTest.java
@@ -9,7 +9,17 @@ public class MathsTest {
 
     @Test
     public void testNumberOfBits() {
-        // power of 2
+        // 2^n
         assertEquals(4, numberOfBits(8));
+        assertEquals(7, numberOfBits(64));
+        assertEquals(11, numberOfBits(1024));
+        // 2^n + 1
+        assertEquals(4, numberOfBits(14));
+        assertEquals(7, numberOfBits(65));
+        assertEquals(11, numberOfBits(1025));
+        // 2^n - 1
+        assertEquals(3, numberOfBits(7));
+        assertEquals(6, numberOfBits(63));
+        assertEquals(10, numberOfBits(1023));
     }
 }
