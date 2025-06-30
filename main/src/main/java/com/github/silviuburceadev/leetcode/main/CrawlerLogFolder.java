b60de1aabb48645add2044b25916193bb4a78023
Silviu BURCEA <silviuburceadev@gmail.com>
2025-06-30 20:50:53 +0300

    Crawler log folder - TDD/GREEN - Fixed impl. for base case

--- main/src/main/java/com/github/silviuburceadev/leetcode/main/CrawlerLogFolder.java
+++ main/src/main/java/com/github/silviuburceadev/leetcode/main/CrawlerLogFolder.java
@@ -1,4 +1,15 @@
 package com.github.silviuburceadev.leetcode.main;
 
+import java.util.Arrays;
+
 public class CrawlerLogFolder {
+
+    public int depth(String[] logs) {
+        return Arrays.stream(logs)
+            .mapToInt(x -> 1)
+            .reduce(0, (a, b) -> {
+                var res = a + b;
+                return Math.max(res, 0);
+            });
+    }
 }
