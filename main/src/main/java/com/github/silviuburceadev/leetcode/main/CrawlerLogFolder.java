daddde9a5749bbfe227d9a0dbf6e477fe6f5fbec
Silviu BURCEA <silviuburceadev@gmail.com>
2025-06-30 22:44:37 +0300

    Crawler log folder - TDD/GREEN - Fixed impl. for base case PWD

--- main/src/main/java/com/github/silviuburceadev/leetcode/main/CrawlerLogFolder.java
+++ main/src/main/java/com/github/silviuburceadev/leetcode/main/CrawlerLogFolder.java
@@ -5,11 +5,12 @@ import java.util.Arrays;
 public class CrawlerLogFolder {
 
     public static final String CWD = "./";
+    public static final String PWD = "../";
 
     public int depth(String[] logs) {
         return Arrays.stream(logs)
             .filter(path -> !path.equals(CWD))
-            .mapToInt(x -> 1)
+            .mapToInt(path -> PWD.equals(path) ? -1 : 1)
             .reduce(0, (a, b) -> {
                 var res = a + b;
                 return Math.max(res, 0);
