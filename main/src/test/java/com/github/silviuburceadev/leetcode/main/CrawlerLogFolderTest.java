ce6a388d7ef643b01888cbac4eee1229ad0ddbe9
Silviu BURCEA <silviuburceadev@gmail.com>
2025-06-30 20:50:53 +0300

    Crawler log folder - TDD/GREEN - Fixed impl. for base case

--- main/src/test/java/com/github/silviuburceadev/leetcode/main/CrawlerLogFolderTest.java
+++ main/src/test/java/com/github/silviuburceadev/leetcode/main/CrawlerLogFolderTest.java
@@ -1,4 +1,20 @@
 package com.github.silviuburceadev.leetcode.main;
 
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
 public class CrawlerLogFolderTest {
+
+    private static final CrawlerLogFolder IMPL = new CrawlerLogFolder();
+
+    @Test
+    void testBaseCaseOneFolder() {
+        GIVEN: {}
+        final var input = new String[] { "d1/" };
+        WHEN: {}
+        final var depth = IMPL.depth(input);
+        THEN: {}
+        assertThat(depth).isEqualTo(1);
+    }
 }
