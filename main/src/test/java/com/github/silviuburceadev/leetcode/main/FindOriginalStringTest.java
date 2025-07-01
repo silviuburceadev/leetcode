1aaef51151de2630bfc15ef0ae3084a841ffdcd8
Silviu BURCEA <silviuburceadev@gmail.com>
2025-07-01 18:24:46 +0300

    Find original string - TDD/REFACTOR - Added more tests as parameterized test

--- main/src/test/java/com/github/silviuburceadev/leetcode/main/FindOriginalStringTest.java
+++ main/src/test/java/com/github/silviuburceadev/leetcode/main/FindOriginalStringTest.java
@@ -1,6 +1,10 @@
 package com.github.silviuburceadev.leetcode.main;
 
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -8,13 +12,22 @@ public class FindOriginalStringTest {
 
     private static final FindOriginalString IMPL = new FindOriginalString();
 
-    @Test
-    void testBaseCase() {
+    public static Stream<Arguments> input() {
+        return Stream.<Arguments>builder()
+                .add(Arguments.of("abbcccc", 5))
+                .add(Arguments.of("abcd", 1))
+                .add(Arguments.of("aaaa", 4))
+                .build();
+    }
+
+    @ParameterizedTest
+    @MethodSource("input")
+    void testBaseCase(String input, int expected) {
         GIVEN: {}
-        final var word = "abbcccc";
+        final var word = input;
         WHEN: {}
         final var result = IMPL.possibleStringCount(word);
         THEN: {}
-        assertThat(result).isEqualTo(5);
+        assertThat(result).isEqualTo(expected);
     }
 }
