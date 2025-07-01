6ae6b7c43aa526b2fd76795558d56d4e42b80751
Silviu BURCEA <silviuburceadev@gmail.com>
2025-07-01 17:51:48 +0300

    Find original string - TDD/RED - Added test for base case

--- main/src/main/java/module-info.java
+++ main/src/main/java/module-info.java
@@ -2,5 +2,4 @@ module com.github.silviuburceadev.leetcode.main {
     requires com.github.silviuburceadev.leetcode.core;
 
     opens com.github.silviuburceadev.leetcode.main;
-    opens com.github.silviuburceadev.leetcode.main.problem_1598;
 }
