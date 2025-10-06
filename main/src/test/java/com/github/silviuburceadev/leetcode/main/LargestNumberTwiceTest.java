c670800e80d924e9c5a733d1931b00574bb3794d
Silviu BURCEA <silviuburceadev@gmail.com>
2025-10-06 22:11:12 +0300

    747 - Tests & Implementation

---
+++ main/src/test/java/com/github/silviuburceadev/leetcode/main/LargestNumberTwiceTest.java
@@ -0,0 +1,47 @@
+package com.github.silviuburceadev.leetcode.main;
+
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class LargestNumberTwiceTest {
+
+    private static final LargestNumberTwice IMPL = new LargestNumberTwice();
+
+    @Test
+    void checkInputIsNotLongEnough() {
+        GIVEN: {}
+        var input = new int[] { 1 };
+
+        WHEN: {}
+        var actual = IMPL.dominantIndex(input);
+
+        THEN: {}
+        assertThat(actual).isEqualTo(-1);
+    }
+
+    @Test
+    void checkLargestIsNotLargeEnough() {
+        GIVEN: {}
+        var input = new int[] { 1, 2, 3, 4 };
+
+        WHEN: {}
+        var actual = IMPL.dominantIndex(input);
+
+        THEN: {}
+        assertThat(actual).isEqualTo(-1);
+    }
+
+    @Test
+    void checkLargestIsLargeEnough() {
+        GIVEN: {}
+        var input = new int[] { 3, 6, 1, 0 };
+
+        WHEN: {}
+        var actual = IMPL.dominantIndex(input);
+
+        THEN: {}
+        assertThat(actual).isEqualTo(1);
+    }
+}
