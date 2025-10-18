998336f92a84b89424ef013459569781e5abcbc4
Silviu BURCEA <silviuburceadev@gmail.com>
2025-10-18 10:49:40 +0300

    Problem 6 implementation

---
+++ main/src/main/java/com/github/silviuburceadev/leetcode/main/ZigZagConversion.java
@@ -0,0 +1,41 @@
+package com.github.silviuburceadev.leetcode.main;
+
+import java.util.stream.IntStream;
+
+public class ZigZagConversion {
+
+    /**
+     * Converts a given string to its zigzag representation.
+     *
+     * @param s       the given numbers
+     * @param numRows the size of the zigzag
+     * @return the zigzag representation
+     */
+    public String convert(String s, int numRows) {
+        if (numRows == 1) return s;
+        if (numRows == 2) {
+            var result = new StringBuilder();
+            var chars = s.toCharArray();
+            for (var i = 0; i < s.length(); i += 2) result.append(chars[i]);
+            for (var i = 1; i < s.length(); i += 2) result.append(chars[i]);
+            return result.toString();
+        }
+
+        var rows = IntStream.range(0, numRows).mapToObj(StringBuilder::new).toList();
+
+        var chars = s.toCharArray();
+        var i = 0;
+        while (i < chars.length) {
+            // down
+            for (var j = 0; j < numRows && i < chars.length; j++) {
+                rows.get(j).append(chars[i++]);
+            }
+            // diagonal-up
+            for (var j = numRows - 2; j > 0 && i < chars.length; j--) {
+                rows.get(j).append(chars[i++]);
+            }
+        }
+
+        return String.join("", rows);
+    }
+}
