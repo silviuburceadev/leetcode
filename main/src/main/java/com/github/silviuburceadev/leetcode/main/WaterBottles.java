d54a3708aec72ba6c880490e2d41c3f97e943e9f
Silviu BURCEA <silviuburceadev@gmail.com>
2025-10-20 20:32:59 +0300

    Problem 3100 implementation

--- main/src/main/java/com/github/silviuburceadev/leetcode/main/WaterBottles.java
+++ main/src/main/java/com/github/silviuburceadev/leetcode/main/WaterBottles.java
@@ -1,16 +1,38 @@
 package com.github.silviuburceadev.leetcode.main;
 
+import com.github.silviuburceadev.leetcode.core.meta.Problem;
+
 public class WaterBottles {
 
+    @Problem(1518)
     public int numWaterBottles(int numBottles, int numExchange) {
         var total = 0;
         var emptyBottles = 0;
 
-        while (numBottles >= 1) {
+        while (numBottles != 0) {
             total += numBottles;
             numBottles += emptyBottles;
             emptyBottles = numBottles % numExchange;
             numBottles /= numExchange;
+        }
+
+        return total;
+    }
+
+    @Problem(3100)
+    public int maxBottlesDrunk(int numBottles, int numExchange) {
+        var total = 0;
+        var emptyBottles = 0;
+        while (numBottles != 0 || emptyBottles >= numExchange) {
+            total += numBottles;
+            emptyBottles += numBottles;
+            numBottles = 0;
+
+            if (emptyBottles >= numExchange) {
+                emptyBottles -= numExchange;
+                numBottles++;
+                numExchange++;
+            }
         }
 
         return total;
