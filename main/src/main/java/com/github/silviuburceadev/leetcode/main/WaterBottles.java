67e5c93d3b567544306c5a4c3a9f78be0b34f213
Silviu BURCEA <silviuburceadev@gmail.com>
2025-10-20 20:37:17 +0300

    Refactoring implementation for problems 1518 and 3100

--- main/src/main/java/com/github/silviuburceadev/leetcode/main/WaterBottles.java
+++ main/src/main/java/com/github/silviuburceadev/leetcode/main/WaterBottles.java
@@ -6,32 +6,26 @@ public class WaterBottles {
 
     @Problem(1518)
     public int numWaterBottles(int numBottles, int numExchange) {
-        var total = 0;
-        var emptyBottles = 0;
-
-        while (numBottles != 0) {
-            total += numBottles;
-            numBottles += emptyBottles;
-            emptyBottles = numBottles % numExchange;
-            numBottles /= numExchange;
-        }
-
-        return total;
+        return drinkExchange(numBottles, numExchange, 0);
     }
 
     @Problem(3100)
     public int maxBottlesDrunk(int numBottles, int numExchange) {
+        return drinkExchange(numBottles, numExchange, 1);
+    }
+
+    private int drinkExchange(int numBottles, int numExchange, int priceIncrease) {
         var total = 0;
         var emptyBottles = 0;
-        while (numBottles != 0 || emptyBottles >= numExchange) {
+        while (numBottles != 0) {
             total += numBottles;
             emptyBottles += numBottles;
             numBottles = 0;
 
-            if (emptyBottles >= numExchange) {
+            while (emptyBottles >= numExchange) {
                 emptyBottles -= numExchange;
                 numBottles++;
-                numExchange++;
+                numExchange += priceIncrease;
             }
         }
 
