ce78f3fd246387e7e943fb1b203de1363b10ab5f
Silviu BURCEA <silviuburceadev@gmail.com>
2025-10-21 08:54:48 +0300

    Problem 2525 implementation

---
+++ main/src/main/java/com/github/silviuburceadev/leetcode/main/BoxCriteria.java
@@ -0,0 +1,22 @@
+package com.github.silviuburceadev.leetcode.main;
+
+public class BoxCriteria {
+    private static final long LARGE = 1_000_000_000;
+    private static final int WIDE = 10_000;
+    private static final int HEAVY = 100;
+
+    public String categorizeBox(int length, int width, int height, int mass) {
+        var isLarge = isLarge(length, width, height);
+        var isWide = length >= WIDE || width >= WIDE || height >= WIDE;
+        var isBulky = isLarge || isWide;
+        var isHeavy = mass >= HEAVY;
+        if (isBulky && isHeavy) return "Both";
+        if (isBulky) return "Bulky";
+        if (isHeavy) return "Heavy";
+        return "Neither";
+    }
+
+    private boolean isLarge(int length, int width, int height) {
+        return (long) length * width * height >= LARGE;
+    }
+}
