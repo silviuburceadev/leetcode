354247f4cf9eaea358eac4ec835d0dcbd7bad46c
Silviu BURCEA <silviuburceadev@gmail.com>
2025-10-24 08:46:18 +0300

    Problem 680 implementation

--- main/src/main/java/com/github/silviuburceadev/leetcode/main/ValidPalindrome.java
+++ main/src/main/java/com/github/silviuburceadev/leetcode/main/ValidPalindrome.java
@@ -3,12 +3,25 @@ package com.github.silviuburceadev.leetcode.main;
 import com.github.silviuburceadev.leetcode.core.Strings;
 import com.github.silviuburceadev.leetcode.core.meta.Problem;
 
-@Problem(125)
 public class ValidPalindrome {
 
     private static final String NON_LETTERS_DIGITS = "[^A-Za-z0-9]+";
 
+    @Problem(125)
     public boolean isPalindrome(String s) {
         return Strings.isPalindrome(s.replaceAll(NON_LETTERS_DIGITS, "").toLowerCase());
+    }
+
+    @Problem(680)
+    public boolean validPalindrome(String s) {
+        var chars = s.toCharArray();
+
+        for (int left = 0, right = chars.length - 1; left < right; left++, right--) {
+            if (chars[left] != chars[right]) {
+                return Strings.isPalindrome(s, left, right - 1) || Strings.isPalindrome(s, left + 1, right);
+            }
+        }
+
+        return true;
     }
 }
