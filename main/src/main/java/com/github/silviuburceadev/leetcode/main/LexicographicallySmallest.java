f2c3509f07b9c27e714628508703d9a9d610189f
Silviu BURCEA <silviuburceadev@gmail.com>
2025-10-25 20:55:47 +0300

    BC 168 - Q1

---
+++ main/src/main/java/com/github/silviuburceadev/leetcode/main/LexicographicallySmallest.java
@@ -0,0 +1,35 @@
+package com.github.silviuburceadev.leetcode.main;
+
+public class LexicographicallySmallest {
+
+    public String lexSmallest(String s) {
+        var chars = s.toCharArray();
+        var min = s;
+
+        for (var i = 0; i < chars.length; i++) {
+            var reversed = min(reverseLeft(s, i), reverseRight(s, i));
+            min = min(min, reversed);
+        }
+
+        return min;
+    }
+
+    private String min(String left, String right) {
+        if (left.compareTo(right) < 0) return left;
+        return right;
+    }
+
+    private String reverseRight(String s, int i) {
+        // reverse right
+        return new StringBuilder().append(s, i, s.length()).reverse()
+                // prepend left
+                .insert(0, s, 0, i).toString();
+    }
+
+    private String reverseLeft(String s, int i) {
+        // reverse left
+        return new StringBuilder(s.substring(0, i)).reverse()
+                // append right
+                .append(s, i, s.length()).toString();
+    }
+}
