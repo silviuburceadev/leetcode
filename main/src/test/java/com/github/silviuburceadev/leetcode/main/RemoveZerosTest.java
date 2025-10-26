8d9ae6d13b92755d93fc2868e5fbd61cac7f1807
Silviu BURCEA <silviuburceadev@gmail.com>
2025-10-26 06:24:29 +0200

    WC 473 - Q1

---
+++ main/src/test/java/com/github/silviuburceadev/leetcode/main/RemoveZerosTest.java
@@ -0,0 +1,31 @@
+package com.github.silviuburceadev.leetcode.main;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class RemoveZerosTest {
+
+    private static final RemoveZeros IMPL = new RemoveZeros();
+
+    @ParameterizedTest
+    @MethodSource("testRemoveZeroesSource")
+    void testRemoveZeroes(long n, long expected) {
+        // when
+        var actual = IMPL.removeZeros(n);
+
+        // then
+        assertThat(actual).isEqualTo(expected);
+    }
+
+    public static Stream<Arguments> testRemoveZeroesSource() {
+        return Stream.<Arguments>builder()
+                .add(Arguments.of(1020030L, 123L))
+                .add(Arguments.of(1L, 1L))
+                .build();
+    }
+}
