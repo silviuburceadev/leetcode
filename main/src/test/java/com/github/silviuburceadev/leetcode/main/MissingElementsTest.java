dd5e8d524656dce311462da8dd151a9ea46bf8dc
Silviu BURCEA <silviuburceadev@gmail.com>
2025-11-02 04:45:50 +0200

    WC 474 - Q1

---
+++ main/src/test/java/com/github/silviuburceadev/leetcode/main/MissingElementsTest.java
@@ -0,0 +1,33 @@
+package com.github.silviuburceadev.leetcode.main;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.List;
+import java.util.stream.Stream;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class MissingElementsTest {
+
+    private static final MissingElements IMPL = new MissingElements();
+
+    @ParameterizedTest
+    @MethodSource("findMissingElementsSource")
+    void findMissingElements(int[] nums, List<Integer> expected) {
+        // when
+        var actual = IMPL.findMissingElements(nums);
+
+        // then
+        assertThat(actual).isEqualTo(expected);
+    }
+
+    public static Stream<Arguments> findMissingElementsSource() {
+        return Stream.<Arguments>builder()
+                .add(Arguments.of(new int[] { 1, 2, 4, 5 }, List.of(3)))
+                .add(Arguments.of(new int[] { 7, 8, 6, 9 }, List.of()))
+                .add(Arguments.of(new int[] { 5, 1 }, List.of(2, 3, 4)))
+                .build();
+    }
+}
