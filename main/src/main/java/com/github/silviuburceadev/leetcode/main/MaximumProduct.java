f3b7b3398cd3ae68be6a46a877faa3ecc06c8eb9
Silviu BURCEA <silviuburceadev@gmail.com>
2025-11-02 05:09:31 +0200

    WC 474 - Q2

---
+++ main/src/main/java/com/github/silviuburceadev/leetcode/main/MaximumProduct.java
@@ -0,0 +1,27 @@
+package com.github.silviuburceadev.leetcode.main;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
+public class MaximumProduct {
+    public static final long MAX = 100_000L;
+
+    public long maxProduct(int[] nums) {
+        var sorted = Arrays.stream(nums).boxed().sorted().collect(Collectors.toCollection(ArrayList::new));
+        var first = sorted.getFirst();
+        var last = sorted.getLast();
+
+        // all are negative, need to select first 2
+        if (last <= 0) return MAX * first * sorted.get(1);
+
+        // all are positive, need to select last 2
+        if (first >= 0) return MAX * sorted.get(sorted.size() - 2) * last;
+
+        // first is negative, last is positive, so we either pick:
+        // first 2, last 2 or first and last
+        var result = Math.max((long)first * sorted.get(1), (long)sorted.get(sorted.size() - 2) * last);
+        result = Math.max(result, (long) -first * last);
+        return MAX * result;
+    }
+}
